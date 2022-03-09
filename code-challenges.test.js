d89ec06d63598d20bfddcb1b49f34fd5383b83fc
mnperrault <perrault.meaghan@gmail.com>
2022-03-09 07:15:50 -0800

    Completed week 3 assessment.

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,8 +15,28 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+// a test/it method, nested within the describe block, that in plain words, describes that the function does.
+
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
+describe("fib",()
+    it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+
+      //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+      expect(fib(num1)).toEqual([1,1,2,3,5,8])
+      expect(fib(num2)).toEqual([1,1,2,3,5,8, 13, 21, 34, 55])
+    })
+  })
+
+// Create a function that declare fib as a variable in an array
+// Use the array to store the Fibonacci to eliminate the need to use auxiliar variables in the code
+// Input 10 following .notation in order to render the first 10 numbers of the Fibonacci output array
+
+var fib = [1, 1, 2];
+for(var i=fib.length; i<10; i++) {
+    fib[i] = fib[i-2] + fib[i-1];
+}
+console.log(fib);
 
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
@@ -24,33 +44,89 @@
 
 // b) Create the function that makes the test pass.
 
+// Create a function that declare fib as a variable in an array
+// Use the array to store the Fibonacci to eliminate the need to use auxiliar variables in the code
+// Input 10 following .notation in order to render the first 10 numbers of the Fibonacci output array
+
+var fib = [1, 1, 2];
+for(var i=fib.length; i<6; i++) {
+    fib[i] = fib[i-2] + fib[i-1];
+}
+console.log(fib);
+
+// Create a function that declare fib as a variable in an array
+// Use the array to store the Fibonacci to eliminate the need to use auxiliar variables in the code
+// Input 10 following .notation in order to render the first 10 numbers of the Fibonacci output array
+
+var fib = [1, 1, 2];
+for(var i=fib.length; i<10; i++) {
+    fib[i] = fib[i-2] + fib[i-1];
+}
+console.log(fib);
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
-
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+describe("oddNumbersInOrder", () => {
+  it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+    const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+    // Expected output: [-9, 7, 9, 199]
+    const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+    expect(oddNumbersInOrder(fullArr1)).toEqual([-9, 7, 9, 199])
+    expect(oddNumbersInOrder(fullArr2)).toEqual([-823, 7, 23])
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+// create a function that takes in an array as a parameter
+// filter every value that isn't a number and every number that is not odd
+// store filtered values on new variable
+// sort the final results
+
+const oddsOnly = (arr) => {
+    let oddNumbers = arr.filter(n => {
+        return n % 2 !== 0 && typeof n === "number"
+    })
+    return oddNumbers.sort((a,b) => a - b)
+}
+
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+describe("sum", ()=>{
+  it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", ()=>{
+    const numbersToAdd1 = [2, 4, 45, 9]
+    // Excpected output: [2, 6, 51, 60]
 
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
+    const numbersToAdd2 = [0, 7, -8, 12]
+    // Expected output: [0, 7, -1, 11]
 
-const numbersToAdd3 = []
+    const numbersToAdd3 = []
 // Expected output: []
-
+    expect(sum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+    expect(sum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+    expect(sum(numbersToAdd3)).toEqual([])
+  })
+})
 
 // b) Create the function that makes the test pass.
+// declare a function of sum
+// sum takes in an array
+// itterate through the array
+// declare the empty array newArr
+// return value of index zero as condtional
+// return the new array of values each containing an accumelating sum
+
+const sumOfArray = (arr) => {
+    let arrAccumulate = 0
+    return arr.map(n => {
+        return arrAccumulate += n
+    })
+}
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,59 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: The keyword 'this' in JS refers to an object.
 
-  Researched answer:
+  Researched answer: In JavaScript, the this keyword refers to an object. Which object depends on how this is being invoked (used or called). The this keyword also refers to different objects depending on how it is used:. In an object method, this refers to the global object.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is a JavaScript library used in web development to build interactive elements on the web.
 
-  Researched answer:
+  Researched answer: React is an open-source JavaScript library for building user interfaces specifically for single-page applications. It's used for handling the view layer for web and mobile apps. React also allows us to create reusable UI components that we can use for multiple apps at once.
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: The render() method is required in a React class component.
 
-  Researched answer:
+  Researched answer: In React, the render() method is the only required method in a class component and is responsible for describing the view to be rendered. Coupled with the clever way React operates around its virtual DOM concept, there are certain subtleties in how this particular method works.
 
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX makes it easier to write and add HTML in React. 
 
-  Researched answer:
+  Researched answer: It was created as a syntactic sugar for React and compiles to HTML so that it can be written in a number of different formats, from plain old HTML to pre-compiled versions of it.
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a software packaging system in JS.
 
-  Researched answer:
+  Researched answer: Yarn is a new package manager that replaces the existing workflow for the npm client or other package managers while remaining compatible with the npm registry. It has the same feature set as existing workflows while operating faster, more securely, and more reliably.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: Anonymous functions in JavaScript are functions that don't have names.
 
-  Researched answer:
+  Researched answer: Anonymous Function is a function that does not have a name associated with it. Normally we use the function keyword before a function name to define a function in JavaScript. However, in anonymous functions in JavaScript, we use only the function word without the function name.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Conditional rendering is the ability to render different user interface (UI) markup if a condition is true or false. This concept is applied often in the following scenarios:. Rendering external data from an API. Rendering elements or components based on a condition for each element or component.
 
-2. Props:
+2. Props: Props are basically kind of global variable or object. They are used to pass information to a component using something called props (stands for properties).
 
-3. JavaScript Events:
+3. JavaScript Events: HTML events are "things" that happen to elements in an HTML page. When JavaScript is used in HTML pages, it can "react" on these events - just like a real-life computer program would react on a live network or mobile device.
 
-4. Object-oriented programming:
+4. Object-oriented programming: Object Oriented Programming (OOP) is a programming paradigm that relies on the concept of classes and objects. It is used to structure a software program into simple, reusable pieces of code blueprints (usually called classes), which are used to create individual instances of objects.
 
-5. Ruby:
+5. Ruby: Ruby is an open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write.
